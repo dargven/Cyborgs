bfa35b07e0383a7711f682cf4464fcb59e2c37d3
EvgeniaSemenova <45089351+evgesnia@users.noreply.github.com>
2023-10-11 18:15:54 +0400

    Bullet
    Пули

---
+++ server/application/modules/DB/Bullet.php
@@ -0,0 +1,10 @@
+<?php
+class Bullet{
+    function __construct($x,$x1,$y,$y1,$speed){
+        $this ->x=$x;
+        $this ->x1=$x1;
+        $this ->y=$y;
+        $this ->y1=$y1;
+        $this ->speed=$speed;
+    }
+}
