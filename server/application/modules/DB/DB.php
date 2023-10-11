c57200bb85d05a4e4e8ba5bee695fe76ed98c01a
EvgeniaSemenova <45089351+evgesnia@users.noreply.github.com>
2023-10-11 18:14:10 +0400

    DB
    Хранение данных

--- server/application/modules/DB/DB.php
+++ server/application/modules/DB/DB.php
@@ -1,6 +1,26 @@
 <?php
+require_once('application/modules/DB/Bullet.php');
+class DB {
+   // $hashPassword=md5($login.’1234’);
 
-class DB
-{
-
+   public function addUser($id,$login,$hashPassword,$token){
+        $this -> UsersData[] = array(
+            'id'=>$id,
+            'login'=>$login,
+            'hashPassword'=>$hashPassword,
+            'token'=>$token);
+    }
+    
+    public function addBullets($bulletId,$Bullet) {
+        $this -> BulletData[] = array(
+            'bulletId'=>$bulletId,
+            'Bullet'=>$Bullet
+        );
+    }
+    
+    public function addScene($scene){
+        $this -> $SceneData[] = array(
+            'scene'=>$scene,
+        );
+    }
 }
