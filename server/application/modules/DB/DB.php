f189cc828e621da91e2a2e3e2b01e8dfe8012775
zapolskiy <zka310504@mail.ru>
2023-10-26 20:37:35 +0400

    DBUser

--- server/application/modules/DB/DB.php
+++ server/application/modules/DB/DB.php
@@ -2,73 +2,35 @@
 
 class DB
 {
-    private array $UsersData = [
-        'Vasya' => [
-            'login' => 'Vasya',
-            'hashPassword' => 'c082282cad5d535061e6205f6e3576a4', // Хэш от логина+пароль(md5('Vasya'.'1234')
-            'token' => null
-        ],
-        'Kirill' => [
-            'login' => 'Kirill',
-            'hashPassword' => 'asd123',
-            'token' => null
-        ]
-    ];
+    //сохраняет соединение с ДБ
+    private $link;
 
-    private $BulletData;
-    private $SceneData;
-
-    public function addUser($login, $hashPassword)
-    {
-        $this->UsersData[$login] = array(
-
-            'login' => $login,
-            'hashPassword' => $hashPassword,
-            'token' => $token
-        );
-
+    //вызов соединения с БД
+    public function __construct(){
+        $this->connect();
     }
 
-    public function isItLogin($login)
-    {
-        $users = $this->UsersData;
-        foreach ($users as $user) {
-            if ($login === $user['login']) {
-                return true;
-            }
+    //ф-ция устанавливает соединение с ДБ
+    private function connect(){
+        $config = require_once 'config.php';
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['db_name'].';charset='.$config['charset'] ;
+        $this->link = new PDO($dsn, $config['username'], $config['password']);
+        return $this;
+    }
+    //ф-ция, которая выполняет запрос
+    public function execute($sql){
+        $sth = $this->link->prepare($sql);
+        return $sth->execute();
+    }
+    //получение рез-та
+    public function query($sql){
+        $sth = $this->link->prepare($sql);
+        $sth->execute();
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+        if ($result === false)
+        {
+            return[];
         }
-        return false;
+        return $result;
     }
-
-    public function addBullets($bulletId, $Bullet)
-    {
-        $this->BulletData = array(
-            'bulletId' => $bulletId,
-            'Bullet' => $Bullet
-        );
-    }
-
-    public function addScene($scene)
-    {
-        $this->SceneData = array(
-            'scene' => $scene,
-        );
-    }
-
-    public function getParamsUser($id, string $params = 'token')
-    {
-        return $this->UsersData[$id][$params];
-
-    }
-
-    public function getUser($login)
-    {
-        return $this->UsersData[$login];
-    }
-
-    public function setValue($login, string $value, $params = 'token'): void
-    {
-        $this->UsersData[$login][$params] = $value;
-    }
-
-}
+}   
---
+++ server/application/modules/DB/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host'=>'localhost',
+    'db_name'=>'Cyborgs',
+    'username'=>'root',
+    'password'=>'',
+    'charset'=>'utf8',
+    
+];
