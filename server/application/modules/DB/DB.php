07cb414ba2057d48fcdab40c9d8f25deca2b45f3
dargven <42145310+dargven@users.noreply.github.com>
2023-10-29 13:16:57 +0400

    Fix

--- server/application/modules/DB/DB.php
+++ server/application/modules/DB/DB.php
@@ -6,79 +6,83 @@ class DB
     private $link;
 
     //вызов соединения с БД
-    public function __construct(){
+    public function __construct()
+    {
         $this->connect();
     }
 
     //ф-ция устанавливает соединение с ДБ
-    private function connect(){
-        $dsn = "mysql:host=localhost;dbname=Cyborgs;charset=utf8" ;
-        $this->link = new PDO($dsn, username:'root',password:'');
-        return $this;
+    private function connect()
+    {
+        $dsn = "mysql:host=localhost;dbname=Cyborgs;charset=utf8";
+        $this->link = new PDO($dsn, 'root', '123');
+        return $this->link;
     }
+
     //ф-ция, которая выполняет запрос
-    public function execute($sql){
+    public function execute($sql)
+    {
         $sth = $this->link->prepare($sql);
         return $sth->execute();
     }
+
     //получение рез-та
-    public function query($sql){
+    public function query($sql)
+    {
         $sth = $this->link->prepare($sql);
         $sth->execute();
         $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-        if ($result === false)
-        {
-            return[];
+        if ($result === false) {
+            return [];
         }
         return $result;
 
     }
-    public function addUser($login,$password,$name,$soname,$token){
-        $db = new DB();
-        $user = $db->execute("INSERT INTO `Users` (login,password,name,soname,token)
+
+    public function addUser($login, $password, $name, $soname, $token)
+    {
+        $user = $this->execute("INSERT INTO `Users` (login,password,name,soname,token)
         VALUES ('$login','$password','$name','$soname','$token')"
         );
     }
-    
-    public function getUserByLogin($login){
-        $db = new DB();
-        $user = $db->query("SELECT * FROM `Users` WHERE login='$login'");
-    }
-    
-    public function getUserById($id){
-        $db = new DB();
-        $user = $db->query("SELECT * FROM `Users` WHERE id='$id'");
-    }
-    
-    public function getUserByToken($token){
-        $db = new DB();
-        $user = $db->query("SELECT * FROM `Users` WHERE token='$token'");
-    }
-    
-    public function updateToken($id,$token){
-        $db = new DB();
-        $user = $db->execute("UPDATE users SET  token='$token' WHERE id='$id'");
+
+    public function getUserByLogin($login)
+    {
+        return $this->query("SELECT * FROM `Users` WHERE login='$login'");
     }
 
-    public function addMessage($user_id,$message){
-        $db = new DB();
-        $message = $db->execute("INSERT INTO `Messages` (user_id, message,created )
+    public function getUserById($id)
+    {
+        return $this->query("SELECT * FROM `Users` WHERE id='$id'");
+    }
+
+    public function getUserByToken($token)
+    {
+         return $this->query("SELECT * FROM `Users` WHERE token='$token'");
+    }
+
+    public function updateToken($id, $token)
+    {
+         return $this->execute("UPDATE users SET  token='$token' WHERE id='$id'");
+    }
+
+    public function addMessage($user_id, $message)
+    {
+        return $this->execute("INSERT INTO `Messages` (user_id, message,created )
         VALUES ('$user_id','$message',now())"
         );
     }
 
-    public function addBullet($id_user,$x,$y,$x1,$y1,$speed){
-        $db = new DB();
-        $bullet = $db->execute("INSERT INTO `Bullet` (id_user, x, y, x1, y1, speed)
+    public function addBullet($id_user, $x, $y, $x1, $y1, $speed)
+    {
+        return $this->execute("INSERT INTO `Bullet` (id_user, x, y, x1, y1, speed)
         VALUES ('$id_user','$x','$y','$x1','$y1','$speed')"
         );
     }
 
-    public function DeleteBullet($id){
-        $db = new DB();
-        $bullet = $db->execute("DELETE * FROM `Bullet` WHERE id='$id'");
+    public function DeleteBullet($id)
+    {
+         return $this->execute("DELETE * FROM `Bullet` WHERE id='$id'");
     }
 } 
 
-$db= new DB();
-$rt = $db->addUser('вася','123','Игорь','Раскольников','1');
