ebfbe289a36b78842b1b3c5456e946f8baedcac4
BulatovSavulii <savabulatov228337@gmail.com>
2023-10-31 19:23:01 +0400

    зона выводит время

--- client/src/components/Game/Zone.tsx
+++ client/src/components/Game/Zone.tsx
@@ -1,11 +1,23 @@
 import { RigidBody, CuboidCollider } from "@react-three/rapier";
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 export interface IZone {
 
 }
-  
+
+/*if (Date.now() - lastShoot.current > FIRE_RATE) {
+    lastShoot.current = Date.now();
+    const newBullet = {
+      id: state.id + "-" + +new Date(),
+      position: vec3(rigidbody.current.translation()),
+      angle,
+      player: state.id,
+    };*/
+
 function Zone  ({ }: IZone)  {
 
+    const [time, setTime]=useState<number>(0);
 
     return (
         
@@ -18,7 +30,15 @@ function Zone  ({ }: IZone)  {
              onIntersectionEnter={(e) => {
                 const data: any = e.other.rigidBody?.userData;
                 if (data.type=="player") {
-                    console.log("1")
+                   setTime (Date.now())
+                }
+            }}
+            onIntersectionExit={(e) => {
+                const data: any = e.other.rigidBody?.userData;
+                if (data.type=="player") {
+                    const left= Date.now()
+                    const score= Math.floor((left - time)/1000)
+                    // console.log(score)
                 }
             }}
                ></CuboidCollider>
