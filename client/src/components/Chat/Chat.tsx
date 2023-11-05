453e1aaa1cf6eeae86dec969c3d59b828490b0ae
fogetmee <tevermoon@gmail.com>
2023-11-05 17:41:50 +0400

    Каркас Чата

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,9 +1,49 @@
-import "Chat"
+import { useState } from "react";
+import "./Chat.css"
 
-const GamePage = () => {
+const Chat = () => {
+    const [showChat, setShowChat] = useState(false)
+
+    //const messagesContainer: HTMLElement | null = document.getElementById('messages'); // Контейнер сообщений — скрипт будет добавлять в него сообщения
+    //let interval: NodeJS.Timer | null = null; // Переменная с интервалом подгрузки сообщений
+    //const sendForm: HTMLFormElement | null = document.getElementById('chat-form'); // Форма отправки
+    //const messageInput: HTMLInputElement | null = document.getElementById('message-text'); // Инпут для текста сообщения
+
+    //function update() {
+    // send_request('load');
+    //}
+
+    //if (interval === null) {
+    //interval = setInterval(update, 500);
+    //}
+
+    //if (sendForm) {
+    //sendForm.onsubmit = function (event: Event) {
+        //send_request('send');
+        //event.preventDefault(); // Используем preventDefault() вместо return false, чтобы остановить стандартную отправку формы
+    //};
+//}
+
     return (
-        <div></div>
+        <div className="chatComponent">
+            <button onClick={() => setShowChat(!showChat)} className="chatBtn">
+                {showChat ? 'Скрыть' : 'Показать'}Чат</button>
+            {showChat && 
+            <div className='chat'>
+                <div className="chat-messages">
+                    <div className="chat-messages__content" id="messages">
+                        Загрузка...
+                    </div>
+                </div>
+                <div className="chat-input">
+                    <form method="post" id="chat-form">
+                        <input type="text" id="mesage-text" className="chat-form__input" placeholder="Введите сообщение"/>
+                        <input type='submit' className="chat-form__submit" value='=>'/>
+                    </form>
+                </div>
+            </div>}
+        </div>
     );
 }
 
-export default GamePage;
+export default Chat;
--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -8,9 +8,8 @@ const NavBar = () => {
 
   return (
     <header className="App-header">
-      <button className='TEST'>Cyborg's</button>
       {store.isAuth() ?
-        <button className="Leave" onClick={() => { }}>Выход</button> :
+        <button className="Leave" onClick={() => {}}>Выход</button> :
         <>
           <NavButton to="/main" text="Играть" />
           <NavButton to="/registration" text="Регистрация" />
