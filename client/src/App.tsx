e1904731bda291d9deb3a5ea0efac690c18c9a1a
GalinAinur <120455369+GalinAinur@users.noreply.github.com>
2023-11-07 16:33:01 +0400

    Fix CSS+Figma->assets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HOST } from "./config";
 import Server from "./modules/Server/Server";
-import LoginPage from "./routes/LoginPage";
 import { Store } from "./modules/Store/Store";
+import LoginPage from "./routes/LoginPage";
 import MainPage from "./routes/MainPage";
 import RegistrationPage from "./routes/RegistrationPage";
 import PrivateRoute from "./components/privateRoute";
 import GamePage from "./routes/GamePage";
-import TestPage from "./routes/TestPage";
 
 export const StoreContext = React.createContext<Store>(null!);
 export const ServerContext = React.createContext<Server>(null!);
@@ -21,12 +20,12 @@ const App: React.FC = () => {
             <StoreContext.Provider value={store}>
                 <ServerContext.Provider value={server}>
                     <Routes>
-                        <Route path="" element={<TestPage/>} />
-                        <Route path="/game" element={<GamePage />} />
+                        <Route path="" element={<MainPage />} />
                         <Route path="/login" element={<LoginPage />} />
                         <Route path="/registration" element={<RegistrationPage />} />
                         <Route element={<PrivateRoute />}>
                             <Route path="/main" element={<MainPage />} />
+                            <Route path="/game" element={<GamePage />} />
                         </Route>
                     </Routes>
                 </ServerContext.Provider>
