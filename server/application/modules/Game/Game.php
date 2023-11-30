7da7be36764490cfdf62bef23646fea226d91ce9
VldmrShuklin <vshuklin13@gmail.com>
2023-11-30 19:05:10 +0400

    spawn coordinates

--- server/application/modules/Game/Game.php
+++ server/application/modules/Game/Game.php
@@ -4,9 +4,49 @@ class Game
 {
     private DB $db;
     private $spawnPoints = [
-        [
-            'x' => 1,
-            'y' => 1
+        0 => [
+            [
+                'x' => 22,
+                'y' => 22
+            ],
+            [
+                'x' => 21.5,
+                'y' => 21.5
+            ],
+            [
+                'x' => 22,
+                'y' => 21.5
+            ],
+            [
+                'x' => 21.5,
+                'y' => 22.5
+            ],
+            [
+                'x' => 22.5,
+                'y' => 22.5
+            ],
+        ],
+        1 => [
+            [
+                'x' => -18,
+                'y' => 14
+            ]
+            [
+                'x' => -17.5,
+                'y' => 13.5
+            ]
+            [
+                'x' => -18.5,
+                'y' => 13.5
+            ]
+            [
+                'x' => -18.5,
+                'y' => 14.5
+            ]
+            [
+                'x' => -17.5,
+                'y' => 14.5
+            ]
         ]
     ];
 
@@ -54,15 +94,15 @@ class Game
 
         return $scene;
     }
-public function getBullets(){
-        return $this->db->getBullets();
-}
-public function getObjects(){
-        return $this->db->getObjects();
-}
-public function getPlayers(){
-        return $this->db->getPlayers();
-}
+    public function getBullets(){
+            return $this->db->getBullets();
+    }
+    public function getObjects(){
+            return $this->db->getObjects();
+    }
+    public function getPlayers(){
+            return $this->db->getPlayers();
+    }
 
     public function spawnPlayers($id, $x, $y)
     {
