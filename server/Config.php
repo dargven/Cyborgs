983bae2252b49e2b1a71d68e63a1b2b14d08b17b
dargven <42145310+dargven@users.noreply.github.com>
2023-12-06 20:28:43 +0400

    Minor rewrite Code

--- server/Config.php
+++ server/Config.php
@@ -29,24 +29,30 @@
 //    ];
 }*/
 
+//DB.CONFIG
+//----------------------------------------------------------------------------//
+
 // PROD
 
-$_ENV['HOST'] = 'server187.hosting.reg.ru';
-$_ENV['PORT'] = 3306;
-$_ENV['USER'] = 'u2333359_dargven';
-$_ENV['PASS'] = 'bAq-UKv-YCK-fxx';
-$_ENV['DB'] = 'u2333359_Cyborgs';
+$_ENV['HOST_PROD'] = 'server187.hosting.reg.ru';
+$_ENV['PORT_PROD'] = 3306;
+$_ENV['USER_PROD'] = 'u2333359_dargven';
+$_ENV['PASS_PROD'] = 'bAq-UKv-YCK-fxx';
+$_ENV['DB_PROD'] = 'u2333359_Cyborgs';
 
 // LOCAL
 
-//$_ENV['HOST'] = '127.0.0.1';
-//$_ENV['PORT'] = 3306;
-//$_ENV['USER'] = 'root';
-//$_ENV['PASS'] = '';
-//$_ENV['DB'] = 'Cyborgs';
+$_ENV['HOST_LC1'] = '127.0.0.1';
+$_ENV['PORT_LC1'] = 3306;
+$_ENV['USER_LC1'] = 'root';
+$_ENV['PASS_LC1'] = '';
+$_ENV['DB_LC1'] = 'Cyborgs';
 
-//$_ENV['HOST'] = 'localhost';
-//$_ENV['PORT'] = 8889;
-//$_ENV['USER'] = 'root';
-//$_ENV['PASS'] = '123';
-//$_ENV['DB'] = 'Cyborgs';
+$_ENV['HOST_LC2'] = 'localhost';
+$_ENV['PORT_LC2'] = 8889;
+$_ENV['USER_LC2'] = 'root';
+$_ENV['PASS_LC2'] = '123';
+$_ENV['DB_LC2'] = 'Cyborgs';
+//END OF DB.CONFIG
+//----------------------------------------------------------------------------//
+
