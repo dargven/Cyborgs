aec56a6e3511558664b858324eedb98622b68553
Dmitry <markelovdima408@gmail.com>
2023-12-08 22:11:53 +0400

    Robo-loading-rotation
    Теперь при загрузке робот совершает вращательное движение вокруг своей Y - оси

--- client/src/components/loading.tsx
+++ client/src/components/loading.tsx
@@ -1,7 +1,12 @@
+import './loading.css';
+
 const LoadingScreen = () => {
     return(
-        <div className="Back">
-            <h3>SCORE</h3>
+        <div className="loading-container">
+            <div className="centered-score">
+                <img className='robot-loading' src="/assets/image/Robot_favikon.png" alt="Loading Robot" />
+            </div>
+            <div className="overlay"></div>
         </div>
     )
 }
