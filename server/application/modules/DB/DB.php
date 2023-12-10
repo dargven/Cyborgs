c7b5809aa952dbc4e197014a4421283fb7829874
dargven <42145310+dargven@users.noreply.github.com>
2023-12-10 19:38:36 +0400

    Fix addUser

--- server/application/modules/DB/DB.php
+++ server/application/modules/DB/DB.php
@@ -2,10 +2,8 @@
 
 class DB
 {
-    //сохраняет соединение с ДБ
     private $pdo;
 
-    //вызов соединения с БД
     public function __construct()
     {
 //----------------------------------------------------------------------------//
@@ -99,7 +97,7 @@ class DB
     public function addUser($login, $hash, $name, $email, $uuid)
     {
         $this->execute(
-            "INSERT INTO users (login,password,name,email,uuid) VALUES (?, ?, ?, ?, ?)",
+            "INSERT INTO users (login,password,name,email, uuid) VALUES (?, ?, ?, ?, ?)",
             [$login, $hash, $name, $email, $uuid]
         );
     }
