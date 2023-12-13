f1c2f674c6eb9eb1d27c339288884fd44c448d48
dargven <42145310+dargven@users.noreply.github.com>
2023-12-13 19:37:54 +0400

    FixOnline(add player, kills, deaths, but need more work)

--- server/application/modules/DB/DB.php
+++ server/application/modules/DB/DB.php
@@ -79,6 +79,11 @@ class DB
         return $this->query("SELECT * FROM users WHERE id=?", [$id]);
     }
 
+    public function getUserByUserId($id)
+    {
+        return $this->query("SELECT * FROM players WHERE user_id=?", [$id]);
+    }
+
     public function getUserByUuid($uuid)
     {
         return $this->query("SELECT * FROM users WHERE uuid=?", [$uuid]);
@@ -149,7 +154,7 @@ FROM bullets as b
 ORDER BY u.bullet_id");
 
     }
-    
+
 
     public function updateScoreInTeam($teamId, $score)
     {
@@ -159,11 +164,9 @@ ORDER BY u.bullet_id");
 
     }
 
-    public function addPlayerToTeam($id, $teamId)
+    public function addPlayer($id, $teamId)
     {
-        $this->execute("INSERT INTO userTeams (user_id, team_id)
-VALUES (?, ?)
-ON DUPLICATE KEY UPDATE team_id = VALUES(team_id)", [$id, $teamId]);
+        $this->execute("INSERT INTO players (user_id, team_id) VALUES (?, ?)", [$id,$teamId]);
     }
 
 
@@ -204,10 +207,12 @@ ON DUPLICATE KEY UPDATE user_id = VALUES(user_id), x = VALUES(x), y = VALUES(y),
       vx = VALUES(vx), vy = VALUES(vy), dx = VALUES(dx), dy = VALUES(dy);
 ", [$id, $x, $y, $vx, $vy, $dx, $dy]);
     }
-    
-    public function setStatus($id, $status){
+
+    public function setStatus($id, $status)
+    {
         $this->execute("UPDATE players SET status = ? WHERE user_id = ?", [$status, $id]);
     }
+
     public function addUserStats($user_id, $kills, $death, $time_in_game, $points)
     {
         $this->execute("INSERT INTO stats (user_id, kills, death, time_in_game, points)
@@ -277,10 +282,11 @@ ON DUPLICATE KEY UPDATE user_id = VALUES(user_id), x = VALUES(x), y = VALUES(y),
     {
         return $this->query("SELECT team_id FROM teams WHERE team_score >= 25");
     }
+
     public function decreaseHp($playerId, $dHp)
     {
-        return $this->execute("UPDATE players SET hp =-? WHERE user_id=?", [$playerId, $dHp]);
+        $this->execute("UPDATE players SET hp =-? WHERE user_id=?", [$playerId, $dHp]);
     }
-    
+
 }
 
