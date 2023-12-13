db9aa3845b0fc676b276c0afe00bf338261a8b29
dargven <42145310+dargven@users.noreply.github.com>
2023-12-13 21:17:45 +0400

    Fix timeOut

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,15 +5,15 @@
 
 // dev2
 //
-// const DOMAIN :string = 'http://localhost';
-// const PORT = 8888;
-// export const HOST :string = `${DOMAIN}:${PORT}`;
+const DOMAIN :string = 'http://localhost';
+const PORT = 8888;
+export const HOST :string = `${DOMAIN}:${PORT}`;
 
 // prod
 // const DOMAIN = 'https://cyborgs-game.ru/server/index.php';
 // const DOMAIN = 'https://api.cyborgs-game.ru/index.php';
 // const DOMAIN = 'https://srv.cyborgs-game.ru/server/index.php'; // ЗДЕСЬ ЛЕЖИТ ТЕСТ И КЛИЕНТА И БЭКА(полурабочая версия)
-const DOMAIN = 'https://test.cyborgs-game.ru/server/index.php'; // Здесь прям сырущая версия клиента и бэка
+// const DOMAIN = 'https://test.cyborgs-game.ru/server/index.php'; // Здесь прям сырущая версия клиента и бэка
 
 
-export const HOST = DOMAIN;
+// export const HOST = DOMAIN;
