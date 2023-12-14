da88d77def1ad823a89efe0e90d6a47f7b3abe08
dargven <42145310+dargven@users.noreply.github.com>
2023-12-14 18:56:40 +0400

    Revert "Delete client/src/components/Game directory"
    This reverts commit 54c4f53f3f94a1a38e33e3832beaa2ad3812e87f.

---
+++ client/src/components/Game/Sprites/MakeSprite.tsx
@@ -0,0 +1,25 @@
+import { NearestFilter } from "three/src/constants";
+import { Vector3 } from "three";
+import { Texture } from "three/src/textures/Texture";
+
+export interface IMakeSprite {
+    texture: Texture;
+    position?: Vector3;
+    scale?: number;
+    rotation?: number;
+}
+
+const MakeSprite = ({ texture, position, scale = 1, rotation = 0 }: IMakeSprite) => {
+    texture.magFilter = NearestFilter;
+    texture.minFilter = NearestFilter;
+    texture.rotation = rotation;
+
+    return (
+        <mesh position={position} scale={[scale, scale, scale]}>
+            <planeGeometry attach="geometry" args={[1, 1, 4]} />
+            <meshStandardMaterial attach="material" map={texture} alphaTest={0.5} />
+        </mesh>
+    )
+}
+
+export default MakeSprite;
