ade538097c479e1aa21206258eb714a8553df32c
BulatovSavulii <118110774+saveliibulatov@users.noreply.github.com>
2023-12-28 18:06:45 +0400

    Rewrite setBullet to shoot

--- server/application/Application.php
+++ server/application/Application.php
@@ -198,7 +198,7 @@ class Application
 
     }
 
-    function setBullet($params)
+    function shoot($params)
     {
         $token = $params['token'];
         $x = $params['x'];
@@ -208,7 +208,7 @@ class Application
         if ($token && ($x || $x == 0) && ($y || $y == 0) && ($vx || $vx == 0) && ($vy || $vy == 0)) {
             $user = $this->user->getUserByToken($token);
             if ($user) {
-                return $this->game->setBullet($user->id, $x, $y, $vx, $vy);
+                return $this->game->shoot($user->id, $x, $y, $vx, $vy);
             }
             return ['error' => 1002];
         }
