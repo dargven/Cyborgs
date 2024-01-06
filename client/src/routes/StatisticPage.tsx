31734506754e50b558e7fecc94f1b223e54bc116
GalinAinur <120455369+GalinAinur@users.noreply.github.com>
2024-01-06 22:52:29 +0400

    Css

--- client/src/routes/StatisticPage.tsx
+++ client/src/routes/StatisticPage.tsx
@@ -1,72 +1,67 @@
 import NavButton from "../components/navButton"
-const StatisticPage=()=>{
+import "./StaticPage.css"
+const StatisticPage = () => {
 
 
-    return(
-    <>
-        <div>
-            Статистика игрока
-        </div>
-        <div>
-            <div>всего игр
-                <div>
-                    228
-                </div>
-            </div>
-            <div>
-                победы
-                <div>
-                    114
-                </div>
-            </div>
-            <div>
-                поражения
-                <div>
-                    114
-                </div>
-            </div>
-            <div>
-                урон за всё время
-                <div>
-                    128.481
-                    <div>
-                        наивысший
+    return (
+        <>
+            <div className="pageWrapper">
+                <div className="pageTitle">Статистика игрока</div>
+                <div className="container">
+                    <div className="static">
                         <div>
-                            2.783
+                            всего игр
+                            <div className="info">228</div>
                         </div>
-
-                    </div>
-                    <div>
-                        средний
                         <div>
-                            321,4
+                            победы
+                            <div className="info">114</div>
+                        </div>
+                        <div>
+                            поражения
+                            <div className="info">114</div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div>
-                убийств за всё время
-                <div>
-                    1.095
-                    <div>
-                        смертей
-                        <div>556</div>
-                    </div>
-                    <div>
-                        k/d
+                    <div className="damage">
+                        урон за все время
                         <div>
-                            1.9
+                            <div className="info-ace">128.481</div>
+                            <div className="info-row">
+                                <div>
+                                    наивысший
+                                    <div className="info">2763</div>
+                                </div>
+                                <div>
+                                    средний
+                                    <div className="info">321.4</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <div className="kill">
+                        убийств за все время
+                        <div>
+                            <div className="info-ace">1.095</div>
+                            <div className="info-row">
+                                <div>
+                                    смертей
+                                    <div className="info">556</div>
+                                </div>
+                                <div>
+                                    k/d
+                                    <div className="info">1.9</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <NavButton
+                        to="/main"
+                        text="вернуться в лобби"
+                        className="Next"></NavButton>
+                
             </div>
-        </div>
-        <NavButton
-        to="/main"
-        text="вернуться в лобби"
-        className="Next"
-        ></NavButton> 
-    </>
-    )
-}
+        </>
+    );
+};
 export default StatisticPage
