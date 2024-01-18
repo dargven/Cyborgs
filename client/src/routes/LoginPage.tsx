bb807af673cc9aa560d1b89bf8c0a6f48a084015
ruthikgame <145386971+ruthikgame@users.noreply.github.com>
2024-01-18 20:55:26 +0400

    tab

--- client/src/routes/LoginPage.tsx
+++ client/src/routes/LoginPage.tsx
@@ -1,79 +1,82 @@
-import { Navigate, useLocation } from "react-router-dom";
-import Loading from "../components/loading";
-import NavButton from "../components/navButton";
-import useEnterKeyHandler from "../hooks/useKeyHandler";
-import useAuth from "../hooks/useAuth";
+import React, { KeyboardEvent } from 'react';
+import { Navigate } from 'react-router-dom';
+import Loading from '../components/loading';
+import NavButton from '../components/navButton';
+import useEnterKeyHandler from '../hooks/useKeyHandler';
+import useAuth from '../hooks/useAuth';
 
-const openEyeIcon = process.env.PUBLIC_URL + "/assets/image/eye-open.png";
-const closeEyeIcon = process.env.PUBLIC_URL + "/assets/image/eye-close.png";
+const openEyeIcon = process.env.PUBLIC_URL + '/assets/image/eye-open.png';
+const closeEyeIcon = process.env.PUBLIC_URL + '/assets/image/eye-close.png';
 
 const LoginPage = () => {
-    const {
-        loginRef,
-        passwordRef,
-        errorRef,
-        loginSuccess,
-        showPassword,
-        isLoading,
-        handleLogin,
-        togglePasswordVisibility,
-      } = useAuth();
+  const {
+    loginRef,
+    passwordRef,
+    errorRef,
+    loginSuccess,
+    showPassword,
+    isLoading,
+    handleLogin,
+    togglePasswordVisibility,
+  } = useAuth();
 
-    useEnterKeyHandler(13, handleLogin);
+  const handleTabKey = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      if (event.target === loginRef.current) {
+        passwordRef.current?.focus();
+      } else if (event.target === passwordRef.current) {
+        loginRef.current?.focus();
+      }
+    }
+  };
 
-    return (
-        <>
-            {isLoading && <Loading/>}
-            <div className="title">
-                КИБОРГИ <br/> ТЕПЕРЬ В 2D
-            </div>
-            <div className="content">
-                <h1>Вход</h1>
-                <div className="input-form">
-                    <input
-                        type="text"
-                        id="login"
-                        name="login"
-                        className="input"
-                        placeholder="Логин"
-                        ref={loginRef}
-                    />
-                    <div className="password-input-container">
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            className="input"
-                            placeholder="Пароль"
-                            ref={passwordRef}
-                        />
-                        <button
-                            className="show-password-button"
-                            onClick={togglePasswordVisibility}
-                        >
-                            <img
-                                src={showPassword ? openEyeIcon : closeEyeIcon}
-                                className="eyeIcon"
-                            />
-                        </button>
-                    </div>
-                    <div ref={errorRef} className="errorDiv"></div>
-                    <div className="PasswordRecovery">
-                        <NavButton
-                            to="/recovery"
-                            text="забыли пароль?"
-                            className="RecoveryButton"
-                        ></NavButton>
-                    </div>
-                    <button onClick={() => 
-                        handleLogin()}>
-                        <h1>Войти</h1>
-                    </button>
-                </div>
-                {loginSuccess ? <Navigate to="/main" replace={true}/> : null}
-            </div>
-        </>
-    );
+  useEnterKeyHandler(13, handleLogin);
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      <div className="title">
+        КИБОРГИ <br /> ТЕПЕРЬ В 2D
+      </div>
+      <div className="content">
+        <h1>Вход</h1>
+        <div className="input-form">
+          <input
+            type="text"
+            id="login"
+            name="login"
+            className="input"
+            placeholder="Логин"
+            ref={loginRef}
+            onKeyDown={handleTabKey} 
+          />
+          <div className="password-input-container">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="input"
+              placeholder="Пароль"
+              ref={passwordRef}
+              onKeyDown={handleTabKey} 
+            />
+            <button className="show-password-button" onClick={togglePasswordVisibility}>
+              <img src={showPassword ? openEyeIcon : closeEyeIcon} className="eyeIcon" />
+            </button>
+          </div>
+          <div ref={errorRef} className="errorDiv"></div>
+          <div className="PasswordRecovery">
+            <NavButton to="/recovery" text="забыли пароль?" className="RecoveryButton"></NavButton>
+          </div>
+          <button onClick={() => handleLogin()}>
+            <h1>Войти</h1>
+          </button>
+        </div>
+        {loginSuccess ? <Navigate to="/main" replace={true} /> : null}
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
--- client/src/routes/RegistrationPage.tsx
+++ client/src/routes/RegistrationPage.tsx
@@ -1,4 +1,5 @@
-import {Navigate} from "react-router-dom";
+import React, { KeyboardEvent } from 'react';
+import { Navigate } from "react-router-dom";
 import useEnterKeyHandler from "../hooks/useKeyHandler";
 import useAuth from "../hooks/useAuth";
 import Loading from "../components/loading";
@@ -6,86 +7,105 @@ import "../Auth.css";
 
 const openEyeIcon = process.env.PUBLIC_URL + "/assets/image/eye-open.png";
 const closeEyeIcon = process.env.PUBLIC_URL + "/assets/image/eye-close.png";
+
 const RegistrationPage = () => {
+  const {
+    loginRef,
+    passwordRef,
+    errorRef,
+    emailRef,
+    nameRef,
+    registrationSuccess,
+    showPassword,
+    isLoading,
+    handleRegistration,
+    togglePasswordVisibility,
+  } = useAuth();
 
-    const {
-        loginRef,
-        passwordRef,
-        errorRef,
-        emailRef,
-        nameRef,
-        registrationSuccess,
-        showPassword,
-        isLoading,
-        handleRegistration,
-        togglePasswordVisibility,
-      } = useAuth();
+   const handleTabKey = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      if (event.target === null) {
+        loginRef.current?.focus();
+      } else if (event.target === loginRef.current) {
+        nameRef.current?.focus();
+      }else if(event.target === nameRef.current) {
+        emailRef.current?.focus();
+      }else if(event.target === emailRef.current) {
+        passwordRef.current?.focus();
+      }else if(event.target === passwordRef.current) {
+        loginRef.current?.focus();
+      }
+    }
+  };
+  useEnterKeyHandler(13, handleRegistration);
 
-    useEnterKeyHandler(13, handleRegistration);
-
-    return (
-        <>
-            {isLoading && <Loading/>}
-            <div className="title">
-                КИБОРГИ <br/> ТЕПЕРЬ В 2D
-            </div>
-            <div className="content">
-                <h1>Регистрация</h1>
-                <div className="input-form">
-                    <input
-                        type="text"
-                        id="login"
-                        name="login"
-                        className="input"
-                        placeholder="Логин"
-                        ref={loginRef}
-                    />
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="input"
-                        placeholder="Имя"
-                        ref={nameRef}
-                    />
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        className="input"
-                        placeholder="Почта"
-                        ref={emailRef}
-                    />
-                    <div className="password-input-container">
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            className="input"
-                            placeholder="Пароль"
-                            ref={passwordRef}
-                        />
-                        <button
-                            className="show-password-button"
-                            onClick={togglePasswordVisibility}
-                        >
-                            <img
-                                src={showPassword ? openEyeIcon : closeEyeIcon}
-                                className="eyeIcon"
-                            />
-                        </button>
-                        <div ref={errorRef} className="errorDiv"></div>
-                    </div>
-                    <button onClick={() => handleRegistration()}>
-                        <h1>Зарегистрироваться</h1>
-                    </button>
-                </div>
-                {registrationSuccess ? (
-                    <Navigate to="/login" replace={true}/>
-                ) : null}
-            </div>
-        </>
-    );
+  return (
+    <>
+      {isLoading && <Loading />}
+      <div className="title">
+        КИБОРГИ <br /> ТЕПЕРЬ В 2D
+      </div>
+      <div className="content">
+        <h1>Регистрация</h1>
+        <div className="input-form">
+          <input
+            type="text"
+            id="login"
+            name="login"
+            className="input"
+            placeholder="Логин"
+            ref={loginRef}
+            onKeyDown={handleTabKey}
+          />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="input"
+            placeholder="Имя"
+            ref={nameRef}
+            onKeyDown={handleTabKey}
+          />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="input"
+            placeholder="Почта"
+            ref={emailRef}
+            onKeyDown={handleTabKey}
+          />
+          <div className="password-input-container">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="input"
+              placeholder="Пароль"
+              ref={passwordRef}
+              onKeyDown={handleTabKey}
+            />
+            <button
+              className="show-password-button"
+              onClick={togglePasswordVisibility}
+            >
+              <img
+                src={showPassword ? openEyeIcon : closeEyeIcon}
+                className="eyeIcon"
+                alt="Eye Icon"
+              />
+            </button>
+            <div ref={errorRef} className="errorDiv" tabIndex={6}></div>
+          </div>
+          <button onClick={() => handleRegistration()} tabIndex={7}>
+            <h1>Зарегистрироваться</h1>
+          </button>
+        </div>
+        {registrationSuccess ? <Navigate to="/login" replace={true} /> : null}
+      </div>
+    </>
+  );
 };
 
 export default RegistrationPage;
